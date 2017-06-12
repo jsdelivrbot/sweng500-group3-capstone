344b6839e40f727b5b77d3453167fa88c1f77113
stupendous-man <idiegoromero@gmail.com>
2017-06-11 21:16:42 -0400

    Testing database logic

--- index.js
+++ index.js
@@ -55,9 +55,9 @@ app.get('/dblogic', function (request, response) {
   });
 });
 
-app.get('/dblogic/record', function (request, response) {
+app.post('/dblogic/record', function (request, response) {
     pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-        client.query('INSERT INTO test_table(id, name) VALUES($1, $2)', [id, name], function(err, result) {
+        client.query('INSERT INTO test_table(id, name) VALUES($1, $2)', [request.id, request.name], function(err, result) {
             done();
             if (err)
             { console.error(err); response.send("Error " + err); }
@@ -66,15 +66,3 @@ app.get('/dblogic/record', function (request, response) {
         });
     });
 });
-//
-// app.post('/dblogic/record', function (request, response) {
-//     pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-//         client.query('INSERT INTO test_table(id, name) VALUES($1, $2)', [request.id, request.name], function(err, result) {
-//             done();
-//             if (err)
-//             { console.error(err); response.send("Error " + err); }
-//             else
-//             { response.render('pages/dblogic', {results: result.rows} ); }
-//         });
-//     });
-// });
