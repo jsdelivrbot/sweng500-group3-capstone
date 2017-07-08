9f4043cd1921879f2ad7e8a794d40716b08c1c7f
stupendous-man <idiegoromero@gmail.com>
2017-07-08 09:02:18 -0400

    Modified index.js

--- index.js
+++ index.js
@@ -256,20 +256,25 @@ app.get('/respondentSearch', function(request, response) {
 
 //TODO: Enable body-parser functionality in heroku
 //TODO: Enable post operation for database updates/inserts
-// var bodyParser = require('body-parser');
-// app.use(bodyParser.json()); //support json encoded bodies
-// app.use(bodyParser.urlencoded({extended: true})); //support encoded bodies
-//
-// app.post('/dblogic', function (request, response) {
-//     pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-//         client.query('INSERT INTO test_table(id, name) VALUES($1, $2)', [request.body.id, request.body.name], function(err, result) {
-//             done();
-//             if (err)
-//             { console.error(err); response.send("Error " + err); }
-//             else
-//             { response.render('pages/dblogic', {results: result.rows} ); }
-//         });
-//     });
-// });
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); //support json encoded bodies
+app.use(bodyParser.urlencoded({extended: true})); //support encoded bodies
+
+app.post('/surveyreportslogin', function (request, response) {
+    // pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    //     client.query('INSERT INTO test_table(id, name) VALUES($1, $2)', [request.body.id, request.body.name], function(err, result) {
+    //         done();
+    //         if (err)
+    //         { console.error(err); response.send("Error " + err); }
+    //         else
+    //         { response.render('pages/dblogic', {results: result.rows} ); }
+    //     });
+    // });
+
+    var username = request.body.username;
+    var password = request.body.password;
+
+    response.send(username + ' ' + password);
+});
 
 
