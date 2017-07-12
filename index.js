dc86eef32ac9e46acd9790e2228dcc5c12dfd82e
jameshuckestein@juno.com <ja638encad>
2017-07-12 14:30:16 -0500

    The logic is inserted into index.js to retrieve all surveys for all respondents for the last 7 days.  None of the other queries are populated yet.

--- index.js
+++ index.js
@@ -251,8 +251,15 @@ app.get('/instructorSearch', function(request, response) {
     pg.connect(process.env.DATABASE_URL, function(err, client, done){
         //There are six different retrieval forms, so use if statements to determine which was executed
         if (typeof request.param('exe1') != 'undefined'){
-            //Execute SQL-1
-            response.render('pages/instructorSearch');
+            //Try to execute get all surveys for all respondents for the past 7 days
+            client.query('SELECT * FROM es_table, eps_table, adresp_table WHERE date >= (now() - INTERVAL '7 DAYS')', function(err, result) {
+                done();
+                if (err) {
+                    console.error(err); response.send("Error " + err);
+                } else {
+                    response.render('pages/instructorSearch', {results: result.rows} );
+                }
+            });
         } else if (typeof request.param('exe2') != 'undefined'){
             //Execute SQL-2
             response.render('pages/instructorSearch');
