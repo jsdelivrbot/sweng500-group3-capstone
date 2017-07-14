563c0d8f94afe943b6d03120b190a503d9a865f4
jameshuckestein@juno.com <ja638encad>
2017-07-14 15:05:21 -0500

    Adding the dummy query back, but not using it yet.

--- index.js
+++ index.js
@@ -251,6 +251,15 @@ app.get('/episodesurvey', function (request, response){
 app.get('/instructorSearch', function(request, response) {
     pg.connect(process.env.DATABASE_URL, function(err, client, done){
         //There are six different retrieval forms, so use if statements to determine which was executed
+        client.query("SELECT * FROM user_table WHERE usernumber='9999'", function(err, result) {
+            done();
+            if (err) {
+                console.error(err); response.send("Error " + err);
+            } else {
+                var dummy = result;     //Now use dummy below for not used "result"
+            }
+        });
+
         if (typeof request.param('exe1') != 'undefined'){
             //Try to execute get all surveys for all respondents for the past 7 days
             client.query("SELECT * FROM es_table, eps_table, adresp_table", function(err, result) {
