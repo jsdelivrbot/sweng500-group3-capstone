b30e2ca697b214004e4227d56b99fe2950086b79
mikelycette <mlycette@gmail.com>
2017-07-19 20:27:02 -0400

    new route for respondent search

--- index.js
+++ index.js
@@ -383,14 +383,14 @@ pg.connect(process.env.DATABASE_URL, function(err, client, done) {
     client.query('SELECT * FROM es_table WHERE usernumber=$1', function(err, result) {
         done();
         if (err) {
-            console.error(err); response.send("Error" + err);
+            console.error(err);
+            response.send("Error" + err);
         } else {
-            response.render('pages/searchResultsRS1', {results: result.rows} );
+            response.render('pages/searchResultsRS1', {results: result.rows});
         }
-
-     };
-     };
+      };
     };
+  };
 });
 
 
