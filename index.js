4be79b7c943ee043818ba77f035ceac415ec217b
wendyhartman <wendy_cain2002@yahoo.com>
2017-07-25 06:18:22 -0400

    Updating survey

--- index.js
+++ index.js
@@ -341,10 +341,10 @@ app.get('/instructorSearch', function (request, response) {
 });
 //respondentSearch code for reports starts here
 
-//app.get('/respondentSearch', function (request, response) {
+app.get('/respondentSearch', function (request, response) {
     //There are six different retrieval forms, so use if statements to determine which was executed
     //and then call the correct searchResultsTemplate based rendering.
-   /* pg.connect(process.env.DATABASE_URL, function (err, client, done) {
+   pg.connect(process.env.DATABASE_URL, function (err, client, done) {
 
         if (typeof request.param('exe7') != 'undefined') {
             //Call the render page searchResultsRespondent1 to do query and output the results for emotional survey.
@@ -357,6 +357,7 @@ app.get('/instructorSearch', function (request, response) {
                     response.render('pages/searchResultsRespondent1', {results: result.rows});
                 }
             });
+            /*
         } else if (typeof request.param('exe8') != 'undefined') {
             //Call the render page searchResultsRespondent2 to do query and output the results for episode survey.
             client.query('SELECT * FROM eps_table', function (err, result) {
